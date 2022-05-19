8c0410758b7d816634f52609d93dcb3e405a5071
aroralanuk <kuarora@ucsd.edu>
2022-05-19 04:51:02 -0700

    cashflow mapping

--- packages/hardhat/contracts/CashflowNFT.sol
+++ packages/hardhat/contracts/CashflowNFT.sol
@@ -17,7 +17,7 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract CashflowNFT is ERC721 {
 
   constructor (string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
-  RedirectAll[] public cashflows;
+  mapping(uint256 => RedirectAll) public cashflows;
 
   function mintWithFlow (
     uint256 tokenId,
